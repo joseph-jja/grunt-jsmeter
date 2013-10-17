e5943279661e179ca7f7687bdc76b7b5b9905d0c
Joe Acosta <y910242@a4440232.federated.fds>
2013-10-17 15:04:49 -0700

    shell file

---
+++ tasks/jsmeter.js
@@ -0,0 +1,55 @@
+/*
+ * grunt-jsmeter
+ * https://github.com/joseph-jja/grunt-jsmeter
+ *
+ * Copyright (c) 2013 Joe Acosta
+ * Licensed under the GPL license.
+ */
+
+'use strict';
+
+var jsmeter = require("jsmeter");
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('jsmeter', 'Grunt plugin to run jsmeter to get metrics out of the code quality', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var data, options; 
+    options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }); //.join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      //src += options.punctuation;
+
+	  console.log(src);
+	  
+      // Write the destination file.
+      //grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      //grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
