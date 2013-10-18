6d46a949932eba61eae57d67f64f47f5fc9a0205
Joe Acosta <y910242@a4440232.federated.fds>
2013-10-18 15:27:51 -0700

    update jsmeter code and get it working in a real environment

--- tasks/jsmeter.js
+++ tasks/jsmeter.js
@@ -1,44 +1,15 @@
-var JSMeterTask = require("../lib/JSMeterTask");
- 
+var JSMeterTask = require("./lib/JSMeterTask");
+
 module.exports = function(grunt) {
 
-	grunt.registerMultiTask('jsmeter', 'Run jsmeter', function() {
-    	var data, meter = require("jsmeter"), 
-    		name, sources, slen, i, result, 
-    		destDir, outputfile, resultData;
+	grunt.registerMultiTask(JSMeterTask.taskName, JSMeterTask.taskDescription, function() {
+    	var meter, sources, destDir;
                 
-        meter = meter['jsmeter'];
-
         sources = this.data.src;
         destDir = this.data.dest;
-        slen = sources.length;
-        for ( i =0; i < slen; i+= 1 ) {  
-            data = grunt.file.read(sources[i]);
-            outputfile = destDir + "/" + sources[i].substring(sources[i].lastIndexOf("/") + 1) + ".log";
-             
-            result = meter.run(data);
-            
-            resultData = "";
-            for (var j = 0; j < result.length; j++) {
-             
-				resultData += " line start: " + result[j].lineStart + "\n";
-				name = ( result[j].name ) ? result[j].name.replace(/^\[\[[^\]]*\]\]\.?/, "") : result[j].name;
-				resultData += "name: " + name + "\n";
-				resultData += " statements: " + result[j].s + "\n";
-				resultData += " lines:      " + result[j].lines + "\n";
-				resultData += " comments:   " + result[j].comments + "\n";
-				resultData += " % comments:   " + Math.round(result[j].comments / (result[j].lines) * 10000)/100 + "%" + "\n";
-				resultData += " branches: " + result[j].b + "\n";
-				resultData += " depth: " + result[j].branchDepth + "\n";
-				resultData += " complexity: " + result[j].complexity + "\n";
-				resultData += " Halstead Volume: " + result[j].halsteadVolume + "\n";
-				resultData += " Halstead Potential: " + result[j].halsteadPotential + "\n";
-				resultData += " Program Level: " + result[j].halsteadLevel + "\n";
-				resultData += " MI Volume: " + result[j].mi + "\n";
-		
-			}   
-			grunt.file.write(outputfile, resultData); 
-        }
+        
+        meter = new JSMeterTask(this, this.options, sources, destDir);
+        meter.run();
     });
     
 };
--- tasks/lib/JSMeterTask.js
+++ tasks/lib/JSMeterTask.js
@@ -1,82 +1,49 @@
-'use strict';
+var grunt = require("grunt");
 
-var jsmeter = require("jsmeter"), 
-	grunt = require("grunt");
+function JSMeterTask(task, options, sources, dest) {
 
-function JSMeterTask(task) {
-
-	this.origTask = task;
-	
-	this.options = task.options;
-
-	this.files = task.filesSrc;
+    this.task = task; 
+    this.options = options; 
+    this.sources = sources;
+    this.dest = dest;
 };
 
-JSMeterTask.prototype = {
-    // Get the party started.
-    run: function() {
- 		var meter, options, result, i;
- 		
- 		options = this.options;
+JSMeterTask.prototype.run = function() {
 
- 		meter = jsmeter['jsmeter'];
- 		
- 		if ( ! this.files ) { 
- 			console.log("No files to meter");
- 			return;
- 		}
-
- 		this.files.src.forEach(function(f) {
- 			f = grunt.file.expand({}, f);
- 			var src = f.filter(function(filepath) {
-				// Warn on and remove invalid source files (if nonull was set).
-				if (!grunt.file.exists(filepath)) {
-				  grunt.log.warn('Source file "' + filepath + '" not found.');
-				  return false;
-				} else {
-				  return true;
-				}
-			}).map(function(filepath) {
-				// Read file source.
-				return grunt.file.read(filepath);
-			});
-			
-            result = meter.run(src);
-			
-			//console.log("here " + options.dest);
-			if ( ! options.dest || options.dest === 'console' ) { 
-				//for (var j = 0; j < result.length; j++) {
- 				//	console.log(result[j].name);
- 				//}
- 				console.log(src);
- 			} else {
- 			//	grunt.file.write(options.dest + "/" + src + "log", result);
- 			}
- 		});
+    var meter, jsmeter = require("jsmeter"), writer, outputfile, dest,
+        render = ( this.dest === 'console' ) ? require("./ConsoleRender") : require("./LogRender");;
+    
+    dest = this.dest;
+    if ( ! this.sources ) { 
+        console.log("No files to meter!");
+        return;
     }
+    meter = jsmeter['jsmeter'];
+    
+    writer = new render();
+    this.sources.forEach(function(f) {
+        var data, results; 
+        
+        data = grunt.file.read(f);
+        results = meter.run(data);
+	    if ( dest !== 'console' ) {
+    	    outputfile = dest + "/" + f.substring(f.lastIndexOf("/") + 1) + ".log";
+            writer.setFilename(outputfile);
+    	}
+    	console.log(dest);
+        writer.writeResults(results); 
+ 
+    });
+    
+};
+
+JSMeterTask.Defaults = {
+    dest: 'console'
 };
  
-// A static attribute holding our defaults so we can test against them.
-JSMeterTask.Defaults = {
-    destination: 'console'
-};
-
 // Some static task information
 JSMeterTask.taskName = "jsmeter";
 JSMeterTask.taskDescription = "Grunt plugin to run jsmeter to get metrics out of the code quality";
- 
-// A static helper method for registering with Grunt
-JSMeterTask.registerWithGrunt = function(grunt) {
- 
-    grunt.registerMultiTask(JSMeterTask.taskName, JSMeterTask.taskDescription, function() {
-		var task, currentTask;
-		
-		currentTask = this;
-		
-        task = new JSMeterTask(currentTask);
- 
-        task.run();
-    });
-};
+
  
 module.exports = JSMeterTask;
