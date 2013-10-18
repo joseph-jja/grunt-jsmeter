105cb693f4d57dcd553e7f9f8453b70e4aa3f96b
Joe Acosta <y910242@a4440232.federated.fds>
2013-10-18 14:34:19 -0700

    add render engines into the mix. Eventually they can be overwritten by the user

---
+++ tasks/lib/ConsoleRender.js
@@ -0,0 +1,33 @@
+
+function ConsoleRender() {}
+
+ConsoleRender.prototype.setFilename = function(filename) {};
+
+ConsoleRender.prototype.writeResults = function(jsmeterResult) {
+
+    var name, resultData = "", result;
+    
+    result = jsmeterResult;
+    
+	for (var j = 0; j < result.length; j++) {
+	 
+		resultData += " line start: " + result[j].lineStart + "\n";
+		name = ( result[j].name ) ? result[j].name.replace(/^\[\[[^\]]*\]\]\.?/, "") : result[j].name;
+		resultData += "name: " + name + "\n";
+		resultData += " statements: " + result[j].s + "\n";
+		resultData += " lines:      " + result[j].lines + "\n";
+		resultData += " comments:   " + result[j].comments + "\n";
+		resultData += " % comments:   " + Math.round(result[j].comments / (result[j].lines) * 10000)/100 + "%" + "\n";
+		resultData += " branches: " + result[j].b + "\n";
+		resultData += " depth: " + result[j].branchDepth + "\n";
+		resultData += " complexity: " + result[j].complexity + "\n";
+		resultData += " Halstead Volume: " + result[j].halsteadVolume + "\n";
+		resultData += " Halstead Potential: " + result[j].halsteadPotential + "\n";
+		resultData += " Program Level: " + result[j].halsteadLevel + "\n";
+		resultData += " MI Volume: " + result[j].mi + "\n";
+
+	}  
+    console.log(resultData);        
+};
+
+module.exports = ConsoleRender;
