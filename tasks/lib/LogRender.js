4e9253fccb3055d864e910dacf4966bc2f2dd558
Joe Acosta <y910242@a4440232.federated.fds>
2013-10-18 14:36:19 -0700

    remove tabs

--- tasks/lib/LogRender.js
+++ tasks/lib/LogRender.js
@@ -3,12 +3,12 @@ var grunt = require("grunt");
 
 function LogRender() {
 
-	this.logfile = "log/jsmeter.log";
+    this.logfile = "log/jsmeter.log";
 
 };
 
 LogRender.prototype.setFilename = function(filename) {
-	this.logfile = filename;
+    this.logfile = filename;
 };
 
 LogRender.prototype.writeResults = function(jsmeterResult) {
@@ -17,24 +17,24 @@ LogRender.prototype.writeResults = function(jsmeterResult) {
     
     result = jsmeterResult;
     
-	for (var j = 0; j < result.length; j++) {
-	 
-		resultData += " line start: " + result[j].lineStart + "\n";
-		name = ( result[j].name ) ? result[j].name.replace(/^\[\[[^\]]*\]\]\.?/, "") : result[j].name;
-		resultData += "name: " + name + "\n";
-		resultData += " statements: " + result[j].s + "\n";
-		resultData += " lines:      " + result[j].lines + "\n";
-		resultData += " comments:   " + result[j].comments + "\n";
-		resultData += " % comments:   " + Math.round(result[j].comments / (result[j].lines) * 10000)/100 + "%" + "\n";
-		resultData += " branches: " + result[j].b + "\n";
-		resultData += " depth: " + result[j].branchDepth + "\n";
-		resultData += " complexity: " + result[j].complexity + "\n";
-		resultData += " Halstead Volume: " + result[j].halsteadVolume + "\n";
-		resultData += " Halstead Potential: " + result[j].halsteadPotential + "\n";
-		resultData += " Program Level: " + result[j].halsteadLevel + "\n";
-		resultData += " MI Volume: " + result[j].mi + "\n";
+    for (var j = 0; j < result.length; j++) {
+     
+        resultData += " line start: " + result[j].lineStart + "\n";
+        name = ( result[j].name ) ? result[j].name.replace(/^\[\[[^\]]*\]\]\.?/, "") : result[j].name;
+        resultData += "name: " + name + "\n";
+        resultData += " statements: " + result[j].s + "\n";
+        resultData += " lines:      " + result[j].lines + "\n";
+        resultData += " comments:   " + result[j].comments + "\n";
+        resultData += " % comments:   " + Math.round(result[j].comments / (result[j].lines) * 10000)/100 + "%" + "\n";
+        resultData += " branches: " + result[j].b + "\n";
+        resultData += " depth: " + result[j].branchDepth + "\n";
+        resultData += " complexity: " + result[j].complexity + "\n";
+        resultData += " Halstead Volume: " + result[j].halsteadVolume + "\n";
+        resultData += " Halstead Potential: " + result[j].halsteadPotential + "\n";
+        resultData += " Program Level: " + result[j].halsteadLevel + "\n";
+        resultData += " MI Volume: " + result[j].mi + "\n";
 
-	}  
+    }  
     grunt.file.write(this.logfile, resultData);        
 };
 
