ded23edbbc80e2f8d045184e45842c8e0b75b415
Joe Acosta <joseph.acosta@macys.com>
2013-11-15 15:48:26 -0800

    update templates and code to render plain html

--- tasks/lib/HTMLRender.js
+++ tasks/lib/HTMLRender.js
@@ -21,18 +21,23 @@ HTMLRender.prototype.getFileExtension = function() {
 
 HTMLRender.prototype.writeline = function(name, value) {
 
-	var data = row["templates/row.tmpl"]({
+    var data = row["templates/row.tmpl"]({
         'name': name,
         'value': value
     });
-    
+
     return data;
 };
 
 HTMLRender.prototype.writeResults = function(jsmeterResult) {
-    var tableData = this.processResults(jsmeterResult);
+    var result, tableData = this.processResults(jsmeterResult);
 
-    grunt.file.write(this.logfile, tableData);
+    result = page["templates/page.tmpl"]({
+        'filename': this.logfile,
+        'tableData': tableData
+    });
+
+    grunt.file.write(this.logfile, result);
 };
 
 module.exports = HTMLRender;
--- templates/page.js
+++ templates/page.js
@@ -21,8 +21,8 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
     + "\n<table>\n";
   if (stack1 = helpers.tableData) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.tableData); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\n</table>\n\n<body>\n</html>";
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</table>\n\n<body>\n</html>\n";
   return buffer;
   });
 
