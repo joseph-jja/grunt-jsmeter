79e0abf220102b0f977a746268a94fb9a5a30ccd
Joseph Acosta <joseph.acosta@macys.com>
2015-03-09 15:22:12 -0700

    update handlebars

--- templates/index.js
+++ templates/index.js
@@ -4,33 +4,22 @@ Handlebars = glob.Handlebars || require('handlebars');
 
 this["GJSM"] = this["GJSM"] || {};
 
-this["GJSM"]["templates/index.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
-
-function program1(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n<li><a href=\""
-    + escapeExpression(((stack1 = (depth0 && depth0.filename)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+this["GJSM"]["templates/index.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+  var lambda=this.lambda, escapeExpression=this.escapeExpression;
+  return "<li><a href=\""
+    + escapeExpression(lambda((depth0 != null ? depth0.filename : depth0), depth0))
     + "\">"
-    + escapeExpression(((stack1 = (depth0 && depth0.filename)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + escapeExpression(lambda((depth0 != null ? depth0.filename : depth0), depth0))
     + "</a>"
-    + escapeExpression(((stack1 = (depth0 && depth0.complexity)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + escapeExpression(lambda((depth0 != null ? depth0.complexity : depth0), depth0))
     + "</li>\n";
-  return buffer;
-  }
-
-  buffer += "<html>\n<title>";
-  if (stack1 = helpers.filename) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.filename); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "<html>\n<title>"
+    + escapeExpression(((helper = (helper = helpers.filename || (depth0 != null ? depth0.filename : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"filename","hash":{},"data":data}) : helper)))
     + "</title>\n<style type=\"text/css\">\n  \n</style>\n<body>\n<ul>\n";
-  stack1 = helpers.each.call(depth0, (depth0 && depth0.filelist), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</ul>\n\n<body>\n</html>\n";
-  return buffer;
-  });
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.filelist : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "</ul>\n\n<body>\n</html>\n";
+},"useData":true});
 
 if (typeof exports === 'object' && exports) {module.exports = this["GJSM"];}
--- templates/page.js
+++ templates/page.js
@@ -4,90 +4,47 @@ Handlebars = glob.Handlebars || require('handlebars');
 
 this["GJSM"] = this["GJSM"] || {};
 
-this["GJSM"]["templates/page.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, options, functionType="function", escapeExpression=this.escapeExpression, self=this, blockHelperMissing=helpers.blockHelperMissing;
-
-function program1(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n        <tr class=\"";
-  if (stack1 = helpers.cssClassName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.cssClassName); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\">\n            <td>";
-  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.name); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.lineStart) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.lineStart); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.statements) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.statements); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.lines) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.lines); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.comments) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.comments); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.commentsPercent) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.commentsPercent); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.branches) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.branches); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.depth) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.depth); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.complexity) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.complexity); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.halsteadVolume) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.halsteadVolume); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.halsteadPotential) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.halsteadPotential); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.progLevel) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.progLevel); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n            <td>";
-  if (stack1 = helpers.miVolume) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.miVolume); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n        <tr>\n    ";
-  return buffer;
-  }
-
-  buffer += "<html>\n<title>";
-  if (stack1 = helpers.filename) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.filename); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</title>\n<style type=\"text/css\">\n  table { border: 1px solid black; }\n  td { border: 1px solid black; }\n  tr.odd { background: #ffe87c; }\n  tr.even { background: #ffffff; }\n  tr.complex td { background: red; color: #ffffff; }\n</style>\n<body>\nSource file: ";
-  if (stack1 = helpers.filename) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = (depth0 && depth0.filename); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\n<br><a href=\"index.html\">Index</a>\n<table>\n<tr>\n<td>name</td>\n<td>line start</td>\n<td>statements</td>\n<td>lines</td>\n<td>comments</td>\n<td>% comments</td>\n<td>branches</td>\n<td>depth</td>\n<td>complexity</td>\n<td>Halstead Volume</td>\n<td>Halstead Potential</td>\n<td>Program Level</td>\n<td>MI Volume</td>\n</tr>\n    ";
-  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data};
-  if (stack1 = helpers.data) { stack1 = stack1.call(depth0, options); }
-  else { stack1 = (depth0 && depth0.data); stack1 = typeof stack1 === functionType ? stack1.call(depth0, options) : stack1; }
+this["GJSM"]["templates/page.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "        <tr class=\""
+    + escapeExpression(((helper = (helper = helpers.cssClassName || (depth0 != null ? depth0.cssClassName : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"cssClassName","hash":{},"data":data}) : helper)))
+    + "\">\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.lineStart || (depth0 != null ? depth0.lineStart : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"lineStart","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.statements || (depth0 != null ? depth0.statements : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"statements","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.lines || (depth0 != null ? depth0.lines : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"lines","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.comments || (depth0 != null ? depth0.comments : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"comments","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.commentsPercent || (depth0 != null ? depth0.commentsPercent : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"commentsPercent","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.branches || (depth0 != null ? depth0.branches : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"branches","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.depth || (depth0 != null ? depth0.depth : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"depth","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.complexity || (depth0 != null ? depth0.complexity : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"complexity","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.halsteadVolume || (depth0 != null ? depth0.halsteadVolume : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"halsteadVolume","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.halsteadPotential || (depth0 != null ? depth0.halsteadPotential : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"halsteadPotential","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.progLevel || (depth0 != null ? depth0.progLevel : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"progLevel","hash":{},"data":data}) : helper)))
+    + "</td>\n            <td>"
+    + escapeExpression(((helper = (helper = helpers.miVolume || (depth0 != null ? depth0.miVolume : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"miVolume","hash":{},"data":data}) : helper)))
+    + "</td>\n        <tr>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, helper, options, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, blockHelperMissing=helpers.blockHelperMissing, buffer = "<html>\n<title>"
+    + escapeExpression(((helper = (helper = helpers.filename || (depth0 != null ? depth0.filename : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"filename","hash":{},"data":data}) : helper)))
+    + "</title>\n<style type=\"text/css\">\n  table { border: 1px solid black; }\n  td { border: 1px solid black; }\n  tr.odd { background: #ffe87c; }\n  tr.even { background: #ffffff; }\n  tr.complex td { background: red; color: #ffffff; }\n</style>\n<body>\nSource file: "
+    + escapeExpression(((helper = (helper = helpers.filename || (depth0 != null ? depth0.filename : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"filename","hash":{},"data":data}) : helper)))
+    + "\n<br><a href=\"index.html\">Index</a>\n<table>\n<tr>\n<td>name</td>\n<td>line start</td>\n<td>statements</td>\n<td>lines</td>\n<td>comments</td>\n<td>% comments</td>\n<td>branches</td>\n<td>depth</td>\n<td>complexity</td>\n<td>Halstead Volume</td>\n<td>Halstead Potential</td>\n<td>Program Level</td>\n<td>MI Volume</td>\n</tr>\n";
+  stack1 = ((helper = (helper = helpers.data || (depth0 != null ? depth0.data : depth0)) != null ? helper : helperMissing),(options={"name":"data","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data}),(typeof helper === functionType ? helper.call(depth0, options) : helper));
   if (!helpers.data) { stack1 = blockHelperMissing.call(depth0, stack1, options); }
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</table>\n\n<body>\n</html>\n";
-  return buffer;
-  });
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "</table>\n\n<body>\n</html>\n";
+},"useData":true});
 
 if (typeof exports === 'object' && exports) {module.exports = this["GJSM"];}
